26c5c6b7639fc02f9a1349423b04085afe62677e
FaridKishiyev <99797424+FaridKishiyev@users.noreply.github.com>
2022-04-01 00:32:09 +0400

    Add files via upload

---
+++ quen/quen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using quen.Queues;
+namespace quen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> que1 = new Queue<string>();
+            Que quer = new Que();
+            que1.Enqueue("ilkin");
+            que1.Enqueue("ehmed");
+            que1.Enqueue("ferid");
+
+            foreach (var item in quer.Reserve(que1))
+            {
+                Console.WriteLine(item);
+            } 
+        }
+    }
+}
---
+++ quen/quen/Queues/Que.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quen.Queues
+{
+    class Que
+    {
+        Queue ques;
+        Stack reverse;
+        Queue<string> reversque;
+        public Que()
+        {
+            ques = new Queue();
+            reverse = new Stack();
+            reversque = new Queue<string>();
+        }
+
+        
+
+        public Queue<string> Reserve(Queue<String> word)
+        {
+            foreach (var item in word)
+            {
+                reverse.Push(item); 
+            }
+
+            foreach (var item in reverse)
+            {
+                reversque.Enqueue((string)item);
+            }
+            return reversque;
+        }
+    }
+}
